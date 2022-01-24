6c3bb60ec71efbfb54d3b2c9edbbf76b0b4d0655
Vikrant1508 <vichourasiya@bestpeers.com>
2022-01-24 19:24:22 +0530

    third commit

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -25,7 +25,7 @@ class TeachersController < ApplicationController
 
     respond_to do |format|
       if @teacher.save
-        format.html { redirect_to teacher_url(@teacher), notice: "Teacher was successfully created." }
+        format.html { redirect_to teacher_url(@teacher), notice: "Result was successfully created." }
         format.json { render :show, status: :created, location: @teacher }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class TeachersController < ApplicationController
   def update
     respond_to do |format|
       if @teacher.update(teacher_params)
-        format.html { redirect_to teacher_url(@teacher), notice: "Teacher was successfully updated." }
+        format.html { redirect_to teacher_url(@teacher), notice: "Result was successfully updated." }
         format.json { render :show, status: :ok, location: @teacher }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class TeachersController < ApplicationController
     @teacher.destroy
 
     respond_to do |format|
-      format.html { redirect_to teachers_url, notice: "Teacher was successfully destroyed." }
+      format.html { redirect_to teachers_url, notice: "Result was successfully destroyed." }
       format.json { head :no_content }
     end
   end
