3508a25df24021c8f29897462d7b37736eaa9199
Vikrant1508 <vichourasiya@bestpeers.com>
2022-01-25 19:03:34 +0530

    fouth commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :teachers
   devise_for :users
+  # get '/users/sign_in'
+  # root to: 'devise/sessions#new', as: 'user'
+  resources :teachers
   get "teachers/index"
   root to: "teachers#index"
 end
